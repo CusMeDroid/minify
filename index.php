140d0c5d10e7648e663f2292947e8df63b514504
CusMeDroid <108187260+CusMeDroid@users.noreply.github.com>
2023-05-09 16:53:09 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+ob_start("minifier");
+function minifier($code) {
+    $search = array(
+         
+        // Remove whitespaces after tags
+        '/\>[^\S ]+/s',
+         
+        // Remove whitespaces before tags
+        '/[^\S ]+\</s',
+         
+        // Remove multiple whitespace sequences
+        '/(\s)+/s',
+         
+        // Removes comments
+        '/<!--(.|\s)*?-->/'
+    );
+    $replace = array('>', '<', '\\1');
+    $code = preg_replace($search, $replace, $code);
+    return $code;
+}
+?>
